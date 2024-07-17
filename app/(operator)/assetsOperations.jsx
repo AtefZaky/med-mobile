1ed11dee910e8dee7881fc3a56a688b0bf04e46d
amrkhaled222 <kamr151515@gmail.com>
2024-07-17 11:59:24 +0300

    operation screen fix bugs

--- app/(operator)/assetsOperations.jsx
+++ app/(operator)/assetsOperations.jsx
@@ -1,114 +1,132 @@
-import { Text, View, ScrollView, Dimensions } from 'react-native'
+import { Text, View, Dimensions } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 
-import React, { Component, useEffect, useState } from 'react'
-import { useGlobalContext } from '../../context/GlobalProvider';
-import { Header, Table } from '../../components';
-import { formatDate } from '../../utils/dateFormater';
-import api from '../../utils/api';
-
+import React, { Component, useEffect, useState } from "react";
+import { useGlobalContext } from "../../context/GlobalProvider";
+import { Header, Loader, Table } from "../../components";
+import { formatDate } from "../../utils/dateFormater";
+import api from "../../utils/api";
+import { ScrollView } from "react-native-virtualized-view";
 
 const AssetsOperations = () => {
-    const {user} = useGlobalContext()
-    const [data, setData] = useState({})
-    const assetsOperationHeader = [
-        "عدد ساعات التشغيل من  بدء التشغيل",
-        "ايقاف",
-        "بدء",
-        "اسم المعدة",
-      ];
+	const { user } = useGlobalContext();
+	const [loader, setLoader] = useState(true);
+	const [data, setData] = useState([]);
+	const assetsOperationHeader = [
+		"عدد ساعات التشغيل من  بدء التشغيل",
+		"ايقاف",
+		"بدء",
+		"اسم المعدة",
+	];
 
-      useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const result = await getData();
-            console.log(result.data);
-            setData(result.data);
-          } catch (error) {
-            console.error('Error fetching data: ', error);
-          }
-        };
-        fetchData()
-      }, [])
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const result = await getData();
 
-      const getData = async () => {
-        const response = await api.get("/assets");
-        return response;
-      };
+				setData(result.data);
+				setLoader(false);
+			} catch (error) {
+				console.error("Error fetching data: ", error);
+			}
+		};
+		fetchData();
+	}, []);
 
-      const handleStart = async(id) => {
-        try {
-          const date = new Date
-          console.log(date)
-          const currentDate = formatDate(date)
-          console.log(currentDate)
-          api.put(`/assets/${id}`, {
-            StatusID: 1,
-            StatusDate: currentDate
-          })  
-        } catch (error) {
-          console.log(error)
-        }
-      }
-      const handleEnd = async(id) => {
-        try {
-          const date = new Date
-          console.log(date)
-          const currentDate = formatDate(date)
-          console.log(currentDate)
-          api.put(`/assets/${id}`, {
-            StatusID: 2,
-            StatusDate: currentDate
-          })  
-        } catch (error) {
-          console.log(error)
-        }
-      }
-      
-      const date= new Date()
+	const getData = async () => {
+		const response = await api.get("/assets");
+		return response;
+	};
 
-      const formatDatee = (date) => {
-        const day = date.getDate();
-        const month = date.getMonth() + 1; // Months are zero-indexed
-        const year = date.getFullYear();
-      
-        // Ensure day and month are two digits
-        const formattedDay = day < 10 ? `0${day}` : day;
-        const formattedMonth = month < 10 ? `0${month}` : month;
-      
-        return `${formattedDay}-${formattedMonth}-${year}`;
-      };
-      const formatedDate = formatDatee(date)
-  return (
-    <>
-    {data ? (
-        <ScrollView>
-          <Header title="تشغيل و ايقاف الوحدات" />
-        <View className="flex justify-center p-4">
-            <Text className="font-tregular text-base text-center">
-                {formatedDate}
-            </Text>
-        </View>
-        <View
-          className="w-full flex py-4"
-          style={{
-              minHeight: Dimensions.get("window").height,
-            }}
-            >
-            <Table assetsOperation={true} header={assetsOperationHeader} data={data.machines} onStartMachine={handleStart} onCloseMachine={handleEnd}/>
-        </View>
-        <Toast />
-      </ScrollView>
-    ) : (
-      <View className="flex justify-center p-4">
-            <Text className="font-tregular text-base text-center">
-                لا يوجد معدات
-            </Text>
-        </View>
-    )}
-      </>
-  );
+	const handleStart = async (id) => {
+		try {
+			const date = new Date();
+
+			const currentDate = formatDate(date);
+
+			api.put(`/assets/${id}`, {
+				StatusID: 1,
+				StatusDate: currentDate,
+			});
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	};
+	const handleEnd = async (id) => {
+		try {
+			const date = new Date();
+
+			const currentDate = formatDate(date);
+
+			api.put(`/assets/${id}`, {
+				StatusID: 2,
+				StatusDate: currentDate,
+			});
+			return true;
+		} catch (error) {
+			console.log(error);
+			return false;
+		}
+	};
+
+	const date = new Date();
+
+	const formatDatee = (date) => {
+		const day = date.getDate();
+		const month = date.getMonth() + 1; // Months are zero-indexed
+		const year = date.getFullYear();
+
+		// Ensure day and month are two digits
+		const formattedDay = day < 10 ? `0${day}` : day;
+		const formattedMonth = month < 10 ? `0${month}` : month;
+
+		return `${formattedDay}-${formattedMonth}-${year}`;
+	};
+	const formatedDate = formatDatee(date);
+	return (
+		<>
+			{!loader ? (
+				<>
+					{data ? (
+						<ScrollView style={{ flexGrow: 1 }}>
+							<Header title="تشغيل و ايقاف الوحدات" />
+							<View className="flex justify-center p-4">
+								<Text className="font-tregular text-base text-center">
+									{formatedDate}
+								</Text>
+							</View>
+							<View
+								className="w-full flex py-4"
+								style={{
+									minHeight: Dimensions.get("window").height,
+									flexGrow: 1,
+								}}>
+								<Table
+									assetsOperation={true}
+									header={assetsOperationHeader}
+									data={data.machines}
+									onStartMachine={handleStart}
+									onCloseMachine={handleEnd}
+								/>
+							</View>
+							<Toast />
+						</ScrollView>
+					) : (
+						<View className="flex justify-center p-4">
+							<Text className="font-tregular text-base text-center">
+								لا يوجد معدات
+							</Text>
+						</View>
+					)}
+				</>
+			) : (
+				<Loader isLoading={loader}></Loader>
+			)}
+		</>
+	);
 };
 
 export default AssetsOperations;
--- components/tableRow.jsx
+++ components/tableRow.jsx
@@ -1,68 +1,107 @@
-import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { getTimeDifference } from '../utils/dateFormater'
+import { View, Text, TouchableOpacity } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getTimeDifference } from "../utils/dateFormater";
+import { ActivityIndicator } from "react-native";
 
-const TableRow = ({item, onStartMachine, onCloseMachine}) => {
-    const [time, setTime] = useState(0)
-    console.log(item)
-    useEffect(() => {
-        const date = new Date
-        console.log(date)
-        setTimeout(() => {
-            const time = getTimeDifference(item.UpdateDate, date)
-            console.log(time)
-            setTime(time)
-        }, 1000);
-    }, [time])
-    console.log(time)
-    
-  return (
-    <View className="flex flex-row justify-between py-2  px-3 items-center">
-    <View className="basis-1/4">
-        <Text className="text-center font-tmedium">
-            {time}
-        </Text>
-    </View>
-    <View className="basis-1/4">
-        <TouchableOpacity
-            onPress={() => {
-                onCloseMachine(item.AssetID);
-            }}
-            className={`${
-                item.IsActive == 2
-                    ? "text-[#F15555] bg-[#F9EAEB]										]"
-                    : "bg-[#F15555] text-white "
-            } px-4 py-1 rounded-md max-w-[70px] ml-3`}>
-            <Text
-                className={`text-center font-tmedium
-                    ${item.IsActive == 2 ? "text-[#F15555] " : " text-white "}`}>
-                ايقاف
-            </Text>
-        </TouchableOpacity>
-    </View>
-    <View className="basis-1/4">
-        <TouchableOpacity
-            className={`${
-                item.IsActive == 1
-                    ? "text-[#019444] bg-[#E8F0EE]"
-                    : "bg-[#019444] text-white "
-            }  px-6 py-1 rounded-md max-w-[70px] ml-3`}
-            onPress={() => {
-                onStartMachine(item.AssetID);
-            }}>
-            <Text
-                className={`font-tmedium ${
-                    item.IsActive == 1 ? "text-[#019444]" : " text-white"
-                }`}>
-                بدء
-            </Text>
-        </TouchableOpacity>
-    </View>
-    <View className="basis-1/5">
-        <Text className="font-tmedium">{item.AssetName}</Text>
-    </View>
-</View>
-  )
-}
+const TableRow = ({ item, onStartMachine, onCloseMachine }) => {
+	const [date, setDate] = useState({ counter: "", startDate: item.UpdateDate });
+	const [active, setActive] = useState(item.StatusID);
+	const [loader, setLoader] = useState({ open: false, close: false });
 
-export default TableRow
+	useEffect(() => {
+		if (active == 1) {
+			const intervalId = setInterval(() => {
+				const time = getTimeDifference(date.startDate);
+				setDate({ ...date, counter: time });
+			}, 1000);
+			return () => clearInterval(intervalId);
+		}
+		if (active == 2) {
+			setDate({ ...date, counter: 0 });
+		}
+		// Cleanup on unmount or time change
+	}, [active]);
+
+	return (
+		<View className="flex flex-row justify-between py-2  px-3 items-center">
+			<View className="basis-1/4">
+				<Text className="text-center font-tmedium">{date.counter}</Text>
+			</View>
+			<View className="basis-1/4">
+				<TouchableOpacity
+					disabled={active == 2}
+					onPress={async () => {
+						setLoader({ ...loader, close: true });
+						const res = await onCloseMachine(item.AssetID);
+						if (res) {
+							setActive(2);
+							setLoader({ ...loader, close: false });
+						}
+					}}
+					className={`${
+						active == 2
+							? "text-[#F15555] bg-[#F9EAEB]										]"
+							: "bg-[#F15555] text-white "
+					} px-4 py-1 rounded-md max-w-[70px] ml-3`}>
+					{loader.close ? (
+						<ActivityIndicator
+							animating={loader.close}
+							color="#fff"
+							size="small"
+							className="ml-2"
+						/>
+					) : (
+						<Text
+							className={`text-center font-tmedium
+                    ${active == 2 ? "text-[#F15555] " : " text-white "}`}>
+							ايقاف
+						</Text>
+					)}
+				</TouchableOpacity>
+			</View>
+			<View className="basis-1/4">
+				<TouchableOpacity
+					className={`${
+						active == 1
+							? "text-[#019444] bg-[#E8F0EE]"
+							: "bg-[#019444] text-white "
+					}  px-6 py-1 rounded-md max-w-[70px] ml-3`}
+					disabled={active == 1}
+					onPress={async () => {
+						setLoader({ ...loader, open: true });
+						const res = await onStartMachine(item.AssetID);
+						if (res) {
+							setLoader(false);
+
+							const nowDate = new Date();
+							nowDate.setHours(nowDate.getHours() + 3);
+							setDate({ counter: 0, startDate: nowDate });
+							setActive(1);
+							setLoader({ ...loader, open: false });
+						}
+					}}>
+					{loader.open ? (
+						<ActivityIndicator
+							animating={loader.open}
+							color="#fff"
+							size="small"
+							className="ml-2"
+						/>
+					) : (
+						<Text
+							className={`font-tmedium ${
+								active == 1 ? "text-[#019444]" : " text-white"
+							}`}>
+							بدء
+						</Text>
+					)}
+				</TouchableOpacity>
+			</View>
+			<View className="basis-1/5">
+				<Text className="font-tmedium">{item.AssetName}</Text>
+			</View>
+		</View>
+	);
+};
+
+export default TableRow;
